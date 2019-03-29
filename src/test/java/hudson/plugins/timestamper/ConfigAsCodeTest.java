9517f39079d31c98ee0cf4a80bfd947191fb9645
Tim Jacomb <tim.jacomb@hmcts.net>
2019-03-29 13:25:33 +0000

    Add JCasC test

---
+++ src/test/java/hudson/plugins/timestamper/ConfigAsCodeTest.java
@@ -0,0 +1,52 @@
+package hudson.plugins.timestamper;
+
+import hudson.ExtensionList;
+import io.jenkins.plugins.casc.ConfigurationAsCode;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.Configurator;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
+import io.jenkins.plugins.casc.model.Mapping;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class ConfigAsCodeTest {
+
+    @Rule public JenkinsConfiguredWithCodeRule r = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public void should_support_configuration_as_code() throws Exception {
+        TimestamperConfig timestamperConfig = TimestamperConfig.get();
+        assertTrue(timestamperConfig.isAllPipelines());
+        assertEquals(timestamperConfig.getElapsedTimeFormat(), "'<b>'HH:mm:ss'</b> '");
+        assertEquals(timestamperConfig.getSystemTimeFormat(), "'<b>'mm:ss'</b> '");
+    }
+
+    @Test
+    @ConfiguredWithCode("configuration-as-code.yml")
+    public void export_configuration() throws Exception {
+        ConfigurationAsCode
+                .get().configure(ConfigAsCodeTest.class.getResource("configuration-as-code.yml").toString());
+
+        ConfigurationAsCode
+                .get().export(System.out);
+        final TimestamperConfig metricsDescriptors = ExtensionList.lookupSingleton(TimestamperConfig.class);
+
+
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        final Configurator c = context.lookupOrFail(TimestamperConfig.class);
+        final CNode node = c.describe(metricsDescriptors, context);
+        assertNotNull(node);
+        final Mapping accessKey = node.asMapping();
+
+        assertEquals(accessKey.getScalarValue("allPipelines"), "true");
+    }
+}
